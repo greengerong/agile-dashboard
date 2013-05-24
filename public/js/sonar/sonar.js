3cec069912021c589e23ff85922e0d44ca7d698a
yqsqq <yqsqq@163.com>
2013-05-24 18:32:35 +0800

    Retrieve data from sonar website then display some metrics on dashboard

--- public/js/sonar/sonar.js
+++ public/js/sonar/sonar.js
@@ -4,11 +4,25 @@ app.directive("sonar", ["proxy", function(proxy){
 		templateUrl: '/html/sonar/sonar.html',
 		replace: true,
 		transclude: false,
-		restrict: 'AE',
-		scope: false,
+		restrict: 'EA',
+		scope: true,
 		controller: function($scope, $element, $attrs, $transclude){
-			console.log("Hi, I am the controller.");
+            var sonarUrl = $scope.dashboardConfig.sonar.url;
+            var resourceKey = $scope.dashboardConfig.sonar.resource;
+            $scope.metrics = [];
+
+            var parseData = function(data){
+                for (var i in data.msr) {
+                    $scope.metrics.push({'name':data.msr[i].key, 'percentage': data.msr[i].val+"%"});
+                }
+                //console.log($scope.metrics);
+                $scope.description = data.description;
+            };
+
+            proxy.get(sonarUrl + '?resource='+resourceKey +'&metrics=coverage,line_coverage,branch_coverage', function(data){
+                //console.log(data);
+                parseData(data[0]);
+            })
 		}
-		
-	};
+    };
 }]);
