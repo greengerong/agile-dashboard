c575e6006b2fa30b9c3f1bdf52ac4cf2a14b7892
=greengerong <=grzx2210@163.com>
2013-05-24 23:44:29 +0800

    add error message box for ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,43 @@
+var app = angular.module("dashboardApp", [])
+    .factory("underscore", ["$window", function ($window) {
+    return $window._;
+}]).value("messageBoxTimer", 3 * 1000).
+    value("messageBoxColor", {"error":"alert-error", "warring":"", "info":"alert-info", "success":"alert-success"}).
+    factory("errorBoxService", ["messageBoxColor", "$timeout", "messageBoxTimer",
+    function (messageBoxColor, $timeout, messageBoxTimer) {
+        var messageBoxContainer = "messageBoxContainer";
+        return {
+            show:function (title, type) {
+                var $container = $("." + messageBoxContainer);
+                if ($container.length < 1) {
+                    var $container = $("<div class='" + messageBoxContainer + "'></div>").prependTo(document.body);
+                }
+                $container.html('<div class="box alert alert-block ' + messageBoxColor[type] + '">' +
+                    '<button class="close" type="button">×</button>' +
+                    '<h4 class="alert-heading">' + title + '</h4>' +
+                    '</div>').find("button").bind("click", function () {
+                        $container.remove();
+                    });
 
-var app = angular.module("dashboardApp",[])
-	.factory("underscore",["$window",function($window){
-		return $window._;
-	}]).factory("proxy",["$http","$window",function($http,$window){
-		return {
-			get: function(url,success,error){
-				$http.get($window.dashboardConfig.proxy + "?url=" + escape(url)).
-				success(success).error(function(){
-					console.log("error");
-				});
-			}
-		};
-	}]);
-	
- function dashboardCtr($scope,$window){
- 	$scope.dashboardConfig = $window.dashboardConfig;
+                $timeout(function () {
+                    $container.remove();
+                }, messageBoxTimer);
+            }
+        };
+    }]).
+    factory("proxy", ["$http", "$window", "errorBoxService", function ($http, $window, errorBoxService) {
+    return {
+        get:function (url, success, error) {
+            $http.get($window.dashboardConfig.proxy + "?url=" + encodeURIComponent(url)).
+                success(success).error(function (error) {
+                    errorBoxService.show(error, "error");
+                });
+        }
+    };
+}]);
 
- }
-app.controller("dashboardCtr",["$scope","$window",dashboardCtr]);
+function dashboardCtr($scope, $window) {
+    $scope.dashboardConfig = $window.dashboardConfig;
+
+}
+app.controller("dashboardCtr", ["$scope", "$window", dashboardCtr]);
