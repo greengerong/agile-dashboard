a6b6e96705ebc313c212f757df2314b508eae955
=greengerong <=grzx2210@163.com>
2013-05-25 00:13:19 +0800

    [zhaxi] add timer for sonar

--- public/js/sonar/sonar.js
+++ public/js/sonar/sonar.js
@@ -1,28 +1,32 @@
-app.directive("sonar", ["proxy", function(proxy){
-	return {
-		priority: 0,
-		templateUrl: '/html/sonar/sonar.html',
-		replace: true,
-		transclude: false,
-		restrict: 'EA',
-		scope: true,
-		controller: function($scope, $element, $attrs, $transclude){
+app.directive("sonar", ["proxy", "timer", "$timeout", function (proxy, timer, $timeout) {
+    return {
+        priority:0,
+        templateUrl:'/html/sonar/sonar.html',
+        replace:true,
+        transclude:false,
+        restrict:'EA',
+        scope:true,
+        controller:function ($scope) {
             var sonarUrl = $scope.dashboardConfig.sonar.url;
             var resourceKey = $scope.dashboardConfig.sonar.resource;
             $scope.metrics = [];
 
-            var parseData = function(data){
+            var parseData = function (data) {
                 for (var i in data.msr) {
-                    $scope.metrics.push({'name':data.msr[i].key, 'percentage': data.msr[i].val+"%"});
+                    $scope.metrics.push({'name':data.msr[i].key, 'percentage':data.msr[i].val + "%"});
                 }
                 //console.log($scope.metrics);
                 $scope.description = data.description;
             };
 
-            proxy.get(sonarUrl + '?resource='+resourceKey +'&metrics=coverage,line_coverage,branch_coverage', function(data){
-                //console.log(data);
-                parseData(data[0]);
-            })
-		}
+            var getSonarInfo = function () {
+                proxy.get(sonarUrl + '?resource=' + resourceKey + '&metrics=coverage,line_coverage,branch_coverage', function (data) {
+                    $timeout(function () {
+                        parseData(data[0])
+                    });
+                })
+            };
+            timer.start(getSonarInfo);
+        }
     };
 }]);
