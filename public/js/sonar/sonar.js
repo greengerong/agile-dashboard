5569839ac1b812d96201d87b5c52d3425904e607
kolyjjj <me.koly.li@gmail.com>
2013-05-24 14:50:51 +0800

    Add the sonar js back and change the attribute to element

--- public/js/sonar/sonar.js
+++ public/js/sonar/sonar.js
@@ -4,17 +4,11 @@ app.directive("sonar", ["proxy", function(proxy){
 		templateUrl: '/html/sonar/sonar.html',
 		replace: true,
 		transclude: false,
-		restrict: 'A',
+		restrict: 'AE',
 		scope: false,
 		controller: function($scope, $element, $attrs, $transclude){
-
-		},
-		compile: function compile(tElement, tAttrs, transclude){
-			return {
-				pre: function preLink(scope, iElement, iAttrs, controller){},
-				post: function postLink(scope, iElement, iAttrs, controller){}	
-			};
-		},
-		link: function postLink(scope, iElement, iAttrs){}
+			console.log("Hi, I am the controller.");
+		}
+		
 	};
 }]);
