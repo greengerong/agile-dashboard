723a8b189d95cafe0aad5e35c89b94580ce8f957
=greengerong <=grzx2210@163.com>
2013-05-28 00:17:32 +0800

    [zhaxi] add total for jira

--- public/js/jira/jira.js
+++ public/js/jira/jira.js
@@ -1,43 +1,54 @@
-app.directive("jira", ["proxy", "timer", "$timeout", "$http", function (proxy, timer, $timeout, $http) {
+app.filter("sum", ["underscore", function (underscore) {
+    return function (items, property) {
+        if (!items) {
+            return 0;
+        }
+
+        return underscore.reduce(items, function (memo, num) {
+            var val = property ? num[property] : num;
+            return memo + val;
+        }, 0);
+    };
+}]).directive("jira", ["proxy", "timer", "$timeout", "$http", function (proxy, timer, $timeout, $http) {
     return {
-        priority: 0,
-        templateUrl: '/html/jira/jira.html',
-        replace: true,
-        transclude: false,
-        restrict: 'EA',
-        scope: true,
-        controller: function ($scope) {
+        priority:0,
+        templateUrl:'/html/jira/jira.html',
+        replace:true,
+        transclude:false,
+        restrict:'EA',
+        scope:true,
+        controller:function ($scope) {
             var jiraHost = $scope.dashboardConfig.jira.url;
-            
+
             var jiraBlocks = $scope.dashboardConfig.jira.blocks;
             var total = 0;
 
-            if(jiraBlocks == null)
+            if (jiraBlocks == null)
                 return;
             var countOfBlocks = jiraBlocks.length;
-            if(countOfBlocks == undefined || countOfBlocks == 0){
+            if (countOfBlocks == undefined || countOfBlocks == 0) {
                 return;
             }
-            
 
-      	     var fillCounts = function(){
-                $scope.rows=[];
-                angular.forEach(jiraBlocks, function(block, blockIndex){
-                var row = []
-                angular.forEach(block.tag, function(tagItem, index){
-                  proxy.get(
-                    jiraHost + "/rest/api/latest/search?jql=priority="+tagItem+"%20AND%20issuetype="+block.type+"&fields=''", 
-                    {Authorization: "Basic eGlhbmdfdGluZzp3ZWxjb21l"},
-                    function (a_index){
-                                return function(data){
-                                      row.push ({key : tagItem, value : data.total, order:a_index});
-                              };
+
+            var fillCounts = function () {
+                $scope.rows = [];
+                angular.forEach(jiraBlocks, function (block, blockIndex) {
+                    var row = []
+                    angular.forEach(block.tag, function (tagItem, index) {
+                        proxy.get(
+                            jiraHost + "/rest/api/latest/search?jql=priority=" + tagItem + "%20AND%20issuetype=" + block.type + "&fields=''",
+                            {Authorization:"Basic eGlhbmdfdGluZzp3ZWxjb21l"},
+                            function (a_index) {
+                                return function (data) {
+                                    row.push({key:tagItem, value:data.total, order:a_index});
+                                };
                             }(index)
-                      );
+                        );
+                    });
+                    $scope.rows.push({key:block.type, value:row, rowIndex:blockIndex});
                 });
-                $scope.rows.push({key:block.type, value:row, rowIndex:blockIndex});
-              });
-        }
+            }
 
             timer.start(fillCounts);
         }
