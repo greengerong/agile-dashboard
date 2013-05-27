84c4ee88aadfed5dd554997d22d5ddbc4cd3f0c7
yqsqq <yqsqq@163.com>
2013-05-27 09:38:09 +0800

    [Qingsong] Adjust the dashboard layout

--- public/js/sonar/sonar.js
+++ public/js/sonar/sonar.js
@@ -9,13 +9,14 @@ app.directive("sonar", ["proxy", "timer", "$timeout", function (proxy, timer, $t
         controller:function ($scope) {
             var sonarUrl = $scope.dashboardConfig.sonar.url;
             var resourceKey = $scope.dashboardConfig.sonar.resource;
-            $scope.metrics = [];
+
 
             var parseData = function (data) {
+                $scope.metrics = [];
                 for (var i in data.msr) {
                     $scope.metrics.push({'name':data.msr[i].key, 'percentage':data.msr[i].val + "%"});
                 }
-                //console.log($scope.metrics);
+                console.log($scope.metrics);
                 $scope.description = data.description;
             };
 
