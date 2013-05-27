7c7a6c68195fa82db1185769f7269884175a4183
=greengerong <=grzx2210@163.com>
2013-05-27 14:45:32 +0800

    [zhaxi] change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 exports.index = function(req, res){
 	
-   res.render('index', { title: 'Project Dashboard',version:"1.0"});
+   res.render('index', { title: 'dashboard',version:"1.0"});
 };
 
 
