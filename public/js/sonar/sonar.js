cc632a97a002625c6abd40b959e0c55ba398b251
yqsqq <yqsqq@163.com>
2013-05-28 09:27:17 +0800

    [Qingsong] Use frmt_val property instead of val, to make display better. Add violations on dashboard

--- public/js/sonar/sonar.js
+++ public/js/sonar/sonar.js
@@ -9,39 +9,61 @@ app.directive("sonar", ["proxy", "timer", "$timeout", function (proxy, timer, $t
         controller: function ($scope) {
 
             var metricsKeyNames = [
-                {"key": "ncloc", "name": "Lines of Code", "suffix": ""},
-                {"key": "coverage", "name": "Coverage", "suffix": "%"},
-                {"key": "line_coverage", "name": "Line Coverage", "suffix": "%"},
-                {"key": "branch_coverage", "name": "Branch Coverage", "suffix": "%"},
-                {"key": "violations_density", "name": "Rules Compliance", "suffix": "%"}
+                {"key": "ncloc", "displayName": "Lines of Code"},
+                {"key": "coverage", "displayName": "Coverage"},
+                {"key": "line_coverage", "displayName": "Line Coverage"},
+                {"key": "branch_coverage", "displayName": "Branch Coverage"},
+                {"key": "violations_density", "displayName": "Rules Compliance"}
             ];
 
-            var parseData = function (data) {
+            var violationKeys = [{"key":"blocker_violations"},
+                {"key":"critical_violations"},
+                {"key":"violations"}];
+
+            var parseMetricsData = function (data) {
                 $scope.metrics = [];
                 for (var i in data.msr) {
-                    $scope.metrics.push({'name': metricsKeyNames[i].name, 'percentage': data.msr[i].val + metricsKeyNames[i].suffix});
+                    $scope.metrics.push({'name': metricsKeyNames[i].displayName, 'percentage': data.msr[i].frmt_val});
                 }
                 $scope.description = data.description;
             };
 
-            var buildMetricsParameters = function () {
-                var metricsParameters = '';
-                for (var i in metricsKeyNames) {
-                    metricsParameters += metricsKeyNames[i].key + ','
-                }
-                return metricsParameters.slice(0, metricsParameters.length - 1);
+            var parseViolations = function(data){
+                $scope.violations = [{"title":"Blocker", "value":data.msr[1].frmt_val},
+                    {"title":"Critical", "value":data.msr[2].frmt_val},
+                    {"title":"Total Violations", "value": data.msr[0].frmt_val}];
+
             };
 
-            var sonarUrl = $scope.dashboardConfig.sonar.url;
+            var buildParameters = function (data) {
+                var parameters = '';
+                for (var i in data) {
+                    parameters += data[i].key + ','
+                }
+                return parameters.slice(0, parameters.length - 1);
+            };
+
+            var sonarBaseUrl = $scope.dashboardConfig.sonar.url;
             var resourceKey = $scope.dashboardConfig.sonar.resource;
-            var sonarRequestUrl = sonarUrl + '?resource=' + resourceKey + '&metrics=' + buildMetricsParameters();
+            var sonarRequestUrlWithResource = sonarBaseUrl + '?resource=' + resourceKey;
+            var sonarRequestUrlForMetrics = sonarRequestUrlWithResource + '&metrics=' + buildParameters(metricsKeyNames);
+            var sonarRequestUrlForViolations = sonarRequestUrlWithResource + '&metrics=' + buildParameters(violationKeys);
+
+            console.log(sonarRequestUrlForViolations);
+
 
             var getSonarInfo = function () {
-                proxy.get(sonarRequestUrl, function (data) {
+                proxy.get(sonarRequestUrlForMetrics, function (data) {
                     $timeout(function () {
-                        parseData(data[0])
+                        parseMetricsData(data[0]);
                     });
-                })
+                });
+
+                proxy.get(sonarRequestUrlForViolations, function(data){
+                    $timeout(function () {
+                        parseViolations(data[0]);
+                    });
+                });
             };
             timer.start(getSonarInfo);
         }
