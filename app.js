32556e063534c14cbbb4710208199934bcfc8227
=greengerong <=grzx2210@163.com>
2013-05-31 09:55:23 +0800

    [zhaxi] style for dashboard

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.post('/proxy/post', proxy.post);
 app.get('/config', routes.config);
 
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Welcome to agile-dashboard,the server listening on port ' + app.get('port'));
 });
